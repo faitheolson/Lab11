6683a0b4fc47c4990529c6ebbf1a21e4fede80a8
Faithe Olson <faithe.olson@gmail.com>
2018-02-05 14:17:25 -0500

    added default class

--- Program.cs
+++ Program.cs
@@ -10,15 +10,15 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-            List<Person> PersonList = new List<Person>();
-            PersonList.Add(new Person("Fred", "1155 Clark"));
-            PersonList.Add(new Student("Bart Simpson","CartoonLand", "Biology", 1975, 1200));
-            PersonList.Add(new Staff("Mrs. Moon", "1100 Republic Dr", "Bingham Elementary", 5000));
+            //List<Person> PersonList = new List<Person>();
+            //PersonList.Add(new Person("Fred", "1155 Woodward"));
+            //PersonList.Add(new Student("Bart Simpson","CartoonLand", "Biology", 1975, 1200));
+            //PersonList.Add(new Staff("Mrs. Moon", "1100 Republic Dr", "Bingham Elementary", 5000));
 
-            foreach (Person item in PersonList)
-            {
-                Console.WriteLine(item.ToString());
-            }
+            //foreach (Person item in PersonList)
+            //{
+            //    Console.WriteLine(item.ToString());
+            //}
 
             
 
